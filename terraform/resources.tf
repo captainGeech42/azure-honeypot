bb514c7e9d145c1cae24263d5df003bb18f906e2
Zander Work <zander@zanderwork.com>
2021-03-07 09:29:46 -0800

    fixed dependency

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "sa" {
   name                = var.azure_sa_name
-  resource_group_name = var.azure_rg_name
-  location            = var.azure_region
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
   account_kind             = "StorageV2"
   account_tier             = "Standard"
