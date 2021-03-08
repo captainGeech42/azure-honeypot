9f76ff64c945ec48d949143550d8e338213d3bec
Zander Work <zander@zanderwork.com>
2021-03-07 20:04:04 -0800

    broke up ad deploy again

--- powershell/deploy-ad.ps1
+++ powershell/deploy-ad.ps1
@@ -5,60 +5,19 @@ param  (
     [Parameter(Mandatory=$true)][System.Security.SecureString]$dsrm_password
 )
 
-workflow Install-AD {
-    param  (
-        [Parameter(Mandatory=$true)][string]$domain,
-        [Parameter(Mandatory=$true)][System.Security.SecureString]$dsrm_password
-    )
+$ErrorActionPreference = "Stop"
 
-    $ErrorActionPreference = "Stop"
+# Install AD DS
+Install-WindowsFeature AD-Domain-Services, RSAT-AD-Tools
 
-    # Install AD DS
-    Install-WindowsFeature AD-Domain-Services, RSAT-AD-Tools
+# Create forest
+Import-Module ADDSDeployment
+Install-ADDSForest -DomainMode "WinThreshold" `
+                   -ForestMode "WinThreshold" `
+                   -SafeModeAdministratorPassword $dsrm_password `
+                   -DomainName $domain `
+                   -InstallDns:$True `
+                   -Force:$True `
+                   -NoRebootOnCompletion
 
-    Import-Module ADDSDeployment
-
-    # Create forest
-    Install-ADDSForest -DomainMode "WinThreshold"
-                       -ForestMode "WinThreshold"
-                       -SafeModeAdministratorPassword $dsrm_password
-                       -DomainName $domain
-                       -InstallDns $True
-                       -Force $True
-                       -NoRebootOnCompletion
-
-    Write-Host "Rebooting"
-    Restart-Computer -Wait
-
-    Import-Module ActiveDirectory
-
-    # Configure password policy
-    $domain = Get-ADDomain
-    Set-ADDefaultDomainPasswordPolicy -Identity $domain
-                                      -ComplexityEnabled $True
-                                      -ReversibleEncryptionEnabled $False
-                                      -MinPasswordAge 1.0:0:0.0
-                                      -MaxPasswordAge 365.0:0:0.0
-
-    # Get names
-    $user_data = (Invoke-RestMethod -Uri "https://randomuser.me/api/?results=48&password=upper,lower,special,14-20&nat=us").results
-
-    # Make users
-    $user_data | %{
-        # Make sure there are no invalid characters in the name
-        $name = "$($_.name.first).$($_.name.last)".ToLower()
-        if ($name.contains("'")) {
-            continue
-        }
-
-        $pw = ConvertTo-SecureString -AsPlainText $_.login.password -Force
-
-        $user = New-ADUser -Enabled $True
-                           -Name $name
-                           -GivenName $_.name.first
-                           -Surname $_.name.last
-                           -AccountPassword $pw
-    }
-}
-
-Install-AD -domain $domain -dsrm_password $dsrm_password
+Write-Host "Please reboot this machine!"
